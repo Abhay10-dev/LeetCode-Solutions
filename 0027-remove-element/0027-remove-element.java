ce7e936315a87fbe168d7ba9a5005e9d2da58f2c
Abhay Vishwakarma <abhayvish10@gmail.com>
2026-08-13 23:13:35 +0530

    Time: 5 ms (0.64%), Space: 43.4 MB (79.76%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+
+        int[] filtered = Arrays.stream(nums)
+                                .filter(i -> i!=val)
+                                .toArray();
+
+        for(int i=0; i<filtered.length; i++){
+            nums[i] = filtered[i];
+        }
+
+        return filtered.length;
+    }
+}
