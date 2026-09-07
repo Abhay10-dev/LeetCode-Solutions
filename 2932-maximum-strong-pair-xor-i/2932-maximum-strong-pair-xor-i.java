49795c539bc3fae4ed4940a88eb1d384c7518ab5
Abhay Vishwakarma <abhayvish10@gmail.com>
2026-09-08 00:01:32 +0530

    Time: 3 ms (48.42%), Space: 44.9 MB (61.19%) - LeetHub

--- 2932-maximum-strong-pair-xor-i/2932-maximum-strong-pair-xor-i.java
+++ 2932-maximum-strong-pair-xor-i/2932-maximum-strong-pair-xor-i.java
@@ -1,30 +1,20 @@
 class Solution {
     public int maximumStrongPairXor(int[] nums) {
-        List<List<Integer>> list = strongPairs(nums);
-
         int maxXOR = 0;
 
-        for(List<Integer> pairs : list){
-            int x = pairs.get(0);
-            int y = pairs.get(1);
-
-            maxXOR = Math.max(maxXOR, x^y);
-        }
-
-        return maxXOR;
-    }
-
-    private List<List<Integer>> strongPairs(int[] nums){
-
-        List<List<Integer>> res = new ArrayList();
-        
         for(int i=0; i<nums.length; i++){
             for(int j=0; j<nums.length; j++){
-                if(Math.abs(nums[i]-nums[j]) <= Math.min(nums[i], nums[j])){
-                    res.add(Arrays.asList(nums[i], nums[j]));
+
+                int x = nums[i];
+                int y = nums[j];
+
+                if(Math.abs(x-y) <= Math.min(x, y)){
+                    maxXOR = Math.max(maxXOR, x^y);
                 }
             }
         }
-        return res;
+            
+
+        return maxXOR;
     }
 }
