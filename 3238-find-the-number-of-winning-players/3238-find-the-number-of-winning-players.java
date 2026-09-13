a752c2a93068a5a057753f6b91da0dcdeba397bc
Abhay Vishwakarma <abhayvish10@gmail.com>
2026-09-14 00:34:10 +0530

    Time: 1 ms (100%), Space: 47.1 MB (37.81%) - LeetHub

---
+++ 3238-find-the-number-of-winning-players/3238-find-the-number-of-winning-players.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int winningPlayerCount(int n, int[][] pick) {
+        if(n==0) return 0;
+        
+        int[][] counts = new int[n][11];
+
+        for (int[] p : pick) {
+            int player = p[0];
+            int color = p[1];
+            counts[player][color]++;
+        }
+
+        int winningPlayers = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int color = 0; color <= 10; color++) {
+                if (counts[i][color] >= i + 1) {
+                    winningPlayers++;
+                    break;
+                }
+            }
+        }
+
+        return winningPlayers;
+    }
+}
