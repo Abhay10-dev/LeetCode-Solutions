c5ac3cb5e518d849e8734100e3f614e5b7ac05cf
Abhay Vishwakarma <abhayvish10@gmail.com>
2026-09-23 00:10:14 +0530

    Time: 523 ms (7.76%), Space: 47.2 MB (81.74%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int num=0;
+
+        for(int i=0; i<=n; i++){
+            boolean isPresent = false;
+            for(int j=0; j<n; j++){
+                if(nums[j] == num){
+                   isPresent = true;
+                }
+            }
+            if(isPresent==false) return num;
+            num++;
+        }
+
+        return -1;
+    }
+}
