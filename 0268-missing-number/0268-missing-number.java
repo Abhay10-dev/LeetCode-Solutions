0b34a1c53a5a817d0c1517acc897238bb9951b86
Abhay Vishwakarma <abhayvish10@gmail.com>
2026-09-23 00:27:05 +0530

    Time: 140 ms (7.76%), Space: 47.1 MB (91.07%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -8,6 +8,7 @@ class Solution {
             for(int j=0; j<n; j++){
                 if(nums[j] == num){
                    isPresent = true;
+                   break;
                 }
             }
             if(isPresent==false) return num;
